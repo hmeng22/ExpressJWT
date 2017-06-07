916c461bd256fb91f3bc2b49e7e47456eac9c969
Hua Meng <hmeng22@users.noreply.github.com>
2017-06-07 13:54:23 -0400

    Update db.js

--- model/db.js
+++ model/db.js
@@ -1,9 +1,23 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_ADDRESS);
+if (process.env.IS_TESTING) {
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>> -------------------- <<<<<<<<<<<<<<<<<<<<<<<<<<<');
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>   TESTING DATABASE   <<<<<<<<<<<<<<<<<<<<<<<<<<<');
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>   TESTING DATABASE   <<<<<<<<<<<<<<<<<<<<<<<<<<<');
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>   TESTING DATABASE   <<<<<<<<<<<<<<<<<<<<<<<<<<<');
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>> -------------------- <<<<<<<<<<<<<<<<<<<<<<<<<<<');
+  mongoose.connect(process.env.MONGODB_ADDRESS_TESTING);
+} else {
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>> -------------------- <<<<<<<<<<<<<<<<<<<<<<<<<<<');
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>> DEV || PROD DATABASE <<<<<<<<<<<<<<<<<<<<<<<<<<<');
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>> DEV || PROD DATABASE <<<<<<<<<<<<<<<<<<<<<<<<<<<');
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>> DEV || PROD DATABASE <<<<<<<<<<<<<<<<<<<<<<<<<<<');
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>> -------------------- <<<<<<<<<<<<<<<<<<<<<<<<<<<');
+  mongoose.connect(process.env.MONGODB_ADDRESS);
+}
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('db : mongodb connected.');
+  console.log('db : mongodb connected. \n\n\n');
 });
