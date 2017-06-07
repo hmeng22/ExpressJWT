45cf655b9e290729c464d8190ebba362ac583cc3
Hua Meng <hmeng22@users.noreply.github.com>
2017-06-07 13:52:40 -0400

    Create tools.js

---
+++ utils/tools.js
@@ -0,0 +1,26 @@
+var winston = require('winston')
+var winstonLogger = new(winston.Logger)({
+  level: process.env.LOG_LEVEL,
+  transports: [new winston.transports.Console({prettyPrint: true, colorize: true})],
+  exitOnError: false
+})
+
+var nodemailer = require('nodemailer')
+var transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_CONFIG_HOST,
+  port: Number(process.env.EMAIL_CONFIG_PORT),
+  secure: process.env.EMAIL_CONFIG_SECURE === "true",
+  auth: {
+    user: process.env.EMAIL_CONFIG_AUTH_USER,
+    pass: process.env.EMAIL_CONFIG_AUTH_PASS
+  },
+  tls: {
+    // do not fail on invalid certs
+    rejectUnauthorized: process.env.EMAIL_CONFIG_TLS_REJECTUNAUTHORIZED === "true"
+  }
+})
+
+module.exports = {
+  log: winstonLogger,
+  transporter: transporter
+}
