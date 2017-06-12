4c00963b3c792b65274e7ba06108742b94dd7a89
Hua Meng <hmeng22@users.noreply.github.com>
2017-06-12 10:03:20 -0400

    Create pm2.config.js

--- pm2.config.js
+++ pm2.config.js
@@ -3,6 +3,8 @@ module.exports = {
     {
       name: "ExpressJWT",
       script: "./bin/www",
+      // cluster mode
+      // instances: 0,
       watch: true,
       ignore_watch: ["node_modules", "./public"]
     }
