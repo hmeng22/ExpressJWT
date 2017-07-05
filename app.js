b8e184a28e2aa8af8243ad92bb28ae1decfb4c6a
Hua Meng <hmeng22@users.noreply.github.com>
2017-07-05 16:35:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var jwt = require('jsonwebtoken');
 var cors = require('cors');
 require('dotenv').config();
 
